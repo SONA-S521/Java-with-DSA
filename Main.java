30a40830e3d608f8f066b75adb5d9be33b24bce7
SONA-S521 <sonasr06@gmail.com>
2026-06-23 14:27:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class A{
+int n1=5;
+int n2=21;
+int multi=n1*n2;
+void display(){
+System.out.println("Result:" +multi);
+}
+}
+class Main{
+public static void main(String[] args){
+A obj=new A();
+obj.display();
+}
+}
